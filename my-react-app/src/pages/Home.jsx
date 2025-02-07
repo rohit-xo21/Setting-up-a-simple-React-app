1c46465d15af6a67902d0f70485c02135de979a8
Rohit <rohithrajka524@gmail.com>
2025-02-07 09:59:02 +0530

    done

---
+++ my-react-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+        <h1>Rohithraj K A</h1>
+        <h3>Contact: <a href="mailto:youremail@example.com">rohithraj.ka.s65@kalvium.community</a></h3>
+        </div>
+    );
+}
+
+export default Home;
